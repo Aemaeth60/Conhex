e44114faf99afcfb0280dbf03e80b8cf84b6fcb7
vince <vincent.yucel@edu.univ-fcomte.fr>
2019-05-01 10:29:30 +0200

    Ajout pour voir la dernière zone prise avant victoire

--- Arena.py
+++ Arena.py
@@ -38,12 +38,12 @@ class Arena():
         board = self.game.getInitBoard()
         it = 0
         while self.game.getGameEnded(board, curPlayer)==0:
+            print("Areas player red : ", self.game.red)
+            print("Areas player blue : ", self.game.blue)
             it+=1
             if verbose:
                 assert(self.display)
                 print("Turn ", str(it), "Player ", str(curPlayer))
-                print("Areas player red : ", self.game.red)
-                print("Areas player blue : ", self.game.blue)
                 self.display(board)
             action = players[curPlayer+1](self.game.getCanonicalForm(board, curPlayer))
 
@@ -55,6 +55,8 @@ class Arena():
             board, curPlayer = self.game.getNextState(board, curPlayer, action)
         if verbose:
             assert(self.display)
+            print("Areas player red : ", self.game.red)
+            print("Areas player blue : ", self.game.blue)
             print("Game over: Turn ", str(it), "Result ", str(self.game.getGameEnded(board, 1)))
             self.display(board)
         return self.game.getGameEnded(board, 1)
