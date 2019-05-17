0a4ab75d15dd196eb02d9ad869fa20e9038b2730
vince <vincent.yucel@edu.univ-fcomte.fr>
2019-05-17 17:08:05 +0200

    Modif pour débug

--- conhex/ConHexGame.py
+++ conhex/ConHexGame.py
@@ -27,7 +27,7 @@ class ConHexGame(Game):
     def getNextState(self, board, player, action):
         # if player takes action on board, return next (board,player)
         # action must be a valid move
-        print("Action in getNextState ", action)
+        print("Score sur getNextState", self.getScore(board, player))
         if action == self.n*self.n:
             return (board, -player)
         b = Board(self.n)
