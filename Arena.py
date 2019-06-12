d40d57ed645a995ab905afdec46528e39c414d79
vince <vincent.yucel@edu.univ-fcomte.fr>
2019-06-12 14:10:51 +0200

    Modif affichage zones dans Arena (mis dans verbose)

--- Arena.py
+++ Arena.py
@@ -41,6 +41,8 @@ class Arena():
             it+=1
             if verbose:
                 assert(self.display)
+                print("Areas player red : ", self.game.r_areas)
+                print("Areas player blue : ", self.game.b_areas)
                 print("Turn ", str(it), "Player ", str(curPlayer))
                 self.display(board)
             action = players[curPlayer+1](self.game.getCanonicalForm(board, curPlayer))
@@ -51,8 +53,6 @@ class Arena():
                 print(action)
                 assert valids[action] >0
             board, curPlayer = self.game.getNextState(board, curPlayer, action)
-            print("Areas player red : ", self.game.r_areas)
-            print("Areas player blue : ", self.game.b_areas)
         if verbose:
             assert(self.display)
             print("Game over: Turn ", str(it), "Result ", str(self.game.getGameEnded(board, 1)))
