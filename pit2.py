f0392a6eb73d915782118bd6f140b73f974196f7
Your Name <you@example.com>
2019-07-10 18:35:19 +0200

    Modif pit pour jouer contre IA

--- pit2.py
+++ pit2.py
@@ -2,7 +2,7 @@ import Arena
 from MCTS import MCTS
 from conhex.ConHexGame import ConHexGame, display
 from conhex.ConHexPlayers import *
-from conhex.keras.NNet import NNetWrapper as NNet
+from conhex.pytorch.NNet import NNetWrapper as NNet
 
 import numpy as np
 from utils import *
@@ -12,7 +12,7 @@ use this script to play any two agents against each other, or play manually with
 any agent.
 """
 
-g = ConHexGame(17, 23,3 )
+g = ConHexGame()
 
 # all players
 rp = RandomPlayer(g).play
@@ -22,7 +22,7 @@ hp = HumanConHexPlayer(g).play
 # nnet players
 n1 = NNet(g)
 #n1.load_checkpoint('./pretrained_models/othello/pytorch/','6x100x25_best.pth.tar')
-n1.load_checkpoint('./temp/','temp.pth.tar')
+n1.load_checkpoint('./temp/','best.pth.tar')
 args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
@@ -34,5 +34,5 @@ n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 #mcts2 = MCTS(g, n2, args2)
 #n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
 
-arena = Arena.Arena(n1p, hp, g, display=display)
-print(arena.playGames(2, verbose=True))
+arena = Arena.Arena(n1p, rp, g, display=display)
+print(arena.playGames(20, verbose=True))
